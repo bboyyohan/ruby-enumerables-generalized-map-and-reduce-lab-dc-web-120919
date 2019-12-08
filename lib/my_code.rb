260780c90e071ca008e974d44d5eaba36296b80b
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-12-08 23:24:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,10 @@
 # Your Code Here
 def map(s)
-  array = []
-  i = 0 
-  while i < x.length 
-    array << (yield(s[i]))
-    i += 1 
-  end array
-end 
+  new = []
+  i = 0
+  while i < s.length
+    new.push(yield(s[i]))
+    i += 1
+  end
+  new
+end
