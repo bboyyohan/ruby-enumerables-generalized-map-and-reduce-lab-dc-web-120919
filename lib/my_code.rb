fad08599cbfbfc2744d4c134fc90d96516a8c874
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-12-08 23:34:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,10 +13,10 @@ def reduce(array, starting_value = 0)
 accum = starting_value
     i = 0
   else
-    accum = s[0]
+    accum = array[0]
     i = 1
   end
-  while i < s.length
+  while i < array.length
     accum = yield(accum, s[i])
     i += 1
   end
